053fd61de58d05749159303249652c2e65a82855
Felipe Bogo <>
2020-10-30 13:15:18 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<app-mission-control></app-mission-control>'
 })
 export class AppComponent {
   title = 'component-interaction';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MissionControlComponent } from './missioncontrol.component';
+import { AstronautComponent } from './astronaut.component';
+import { MissionService } from './mission.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MissionControlComponent,
+    AstronautComponent
   ],
   imports: [
     BrowserModule
